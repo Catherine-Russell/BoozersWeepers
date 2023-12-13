e3b61e74201ed0cc1d2838157e975e0851b2fca6
Ben <benjdixon96@yahoo.co.uk>
2023-12-13 16:44:06 +0000

    addednavBar

---
+++ frontend/src/components/NavBar/NavBar.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import './NavBar.css';
+
+
+
+
+const NavBar = () =>{
+  const [token, setToken] = useState(window.localStorage.getItem("token"))
+
+
+  const logout = () => {
+    window.localStorage.removeItem("token")
+
+  }
+
+
+
+  return(
+    <div>
+      <div className='Navbar'>
+        <a href='/login' className='txt'>login</a>
+        <a href={`/newWager/${token}`} className='txt'>make a wager with yourself</a>
+        <a href={`/signup`} className='txt'>signup page</a>
+
+        
+      </div>
+
+
+
+
+    </div>
+  )
+
+
+
+
+
+
+
+
+
+}
+export default NavBar
--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Post from '../post/Post'
+import NavBar from '../NavBar/NavBar';
 
 const Feed = ({ navigate }) => {
   const [posts, setPosts] = useState([]);
@@ -30,6 +31,7 @@ const Feed = ({ navigate }) => {
     if(token) {
       return(
         <>
+           <NavBar/>
           <h2>Posts</h2>
             <button onClick={logout}>
               Logout
