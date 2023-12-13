4b382781d1ecb3d7c2d2b64656aa8677e2456644
Ben <benjdixon96@yahoo.co.uk>
2023-12-13 12:00:47 +0000

    adds controler and route for index wager

--- api/routes/wagers.js
+++ api/routes/wagers.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const WagersController = require("../controllers/wagers");
 
 router.post("/", WagersController.Create);
+router.get("/", WagersController.Index);
+
 
 module.exports = router;
