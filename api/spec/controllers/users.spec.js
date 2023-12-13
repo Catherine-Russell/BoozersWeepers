d2d25cd802d125f9520a1b3b1f11393daa3b0698
Sam Ford <samueljamesford@googlemail.com>
2023-12-13 17:36:46 +0000

    Great use of time on creating tests

--- api/spec/controllers/users.spec.js
+++ api/spec/controllers/users.spec.js
@@ -8,9 +8,29 @@ const secret = process.env.JWT_SECRET;
 let token;
 
 describe("/users", () => {
+  beforeAll( async () => {
+    let user1 = new User({email: "poppy@email.com",username: "mrstest", password: "1234"});
+    let user2 = new User({email: "cat@email.com",username: "mrtest", password: "1234"});
+    await user1.save();
+    await user2.save();
+  
+    token = JWT.sign({
+    user_id: user1.id,
+    // Backdate this token of 5 minutes
+    iat: Math.floor(Date.now() / 1000) - (5 * 60),
+    // Set the JWT token to expire in 10 minutes
+    exp: Math.floor(Date.now() / 1000) + (10 * 60)
+    }, secret);
+  });
   beforeEach( async () => {
     await User.deleteMany({});
+    console.log("Deleting before")
   });
+  afterEach( async () => {
+
+    await User.deleteMany({});
+  })
+
 
   describe("POST, when email and password are provided", () => {
     test("the response code is 201", async () => {
@@ -81,73 +101,3 @@ describe("/users", () => {
     });
   })})
 
-// Tests Index
-describe("GET, when token is present", () => {
-  
-  beforeAll( async () => {
-    let user1 = new User({email: "poppy@email.com",username: "mrstest", password: "1234"});
-    let user2 = new User({email: "cat@email.com",username: "mrtest", password: "1234"});
-    await user1.save();
-    await user2.save();
-  
-    token = JWT.sign({
-    user_id: user1.id,
-    // Backdate this token of 5 minutes
-    iat: Math.floor(Date.now() / 1000) - (5 * 60),
-    // Set the JWT token to expire in 10 minutes
-    exp: Math.floor(Date.now() / 1000) + (10 * 60)
-    }, secret);
-  });
-
-  afterAll(async () => {
-    await User.deleteMany({});
-  });
-
-  test("returns every user in the database", async () => {
-    let response = await request(app)
-      .get("/userdata")
-      .set("Authorization", `Bearer ${token}`)
-      .send({token: token});
-    let usernames= response.body.users.map((user) => ( user.username ));
-    expect(usernames).toEqual(["mrstest", "mrtest"]);
-  })
-
-  test("the response code is 200", async () => {
-
-    let response = await request(app)
-      .get("/userdata")
-      .set("Authorization", `Bearer ${token}`)
-      .send({token: token});
-    expect(response.status).toEqual(200);
-  })
-
-  test("returns a new token", async () => {
-    let response = await request(app)
-      .get("/userdata")
-      .set("Authorization", `Bearer ${token}`)
-      .send({token: token});
-    let newPayload = JWT.decode(response.body.token, process.env.JWT_SECRET);
-    let originalPayload = JWT.decode(token, process.env.JWT_SECRET);
-    expect(newPayload.iat > originalPayload.iat).toEqual(true);
-  })
-})
-
-describe("GET, when token is missing", () => {
-  test("returns no posts", async () => {
-    let response = await request(app)
-      .get("/userdata");
-    expect(response.body.users).toEqual(undefined);
-  })
-
-  test("the response code is 401", async () => {
-    let response = await request(app)
-      .get("/userdata");
-    expect(response.status).toEqual(401);
-  })
-
-  test("does not return a new token", async () => {
-    let response = await request(app)
-      .get("/userdata");
-    expect(response.body.token).toEqual(undefined);
-  })
-})
