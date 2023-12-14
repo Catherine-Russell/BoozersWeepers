0f3b598be45f5802425e40587c701210eadac979
Ben <benjdixon96@yahoo.co.uk>
2023-12-14 13:28:02 +0000

    added a list of wager requests to profile Page

--- frontend/src/components/myAccountPage/myAccountPageComponents/IncomingWagers.js
+++ frontend/src/components/myAccountPage/myAccountPageComponents/IncomingWagers.js
@@ -1,23 +1,25 @@
 import React, { useEffect, useState } from 'react';
 
-const IncomingWagers = ({ navigate }, props) => {
+
+const IncomingWagers = (props, { navigate }) => {
   const [token, setToken] = useState(window.localStorage.getItem("token"));
-  const [data, setData] = use
+  const [data, setData] = useState(props.data)
+  
 
     if(token) {
       return(
-        <div id="incoming-wagers">
-          <h6>{props.id} the person with this id wants to wager</h6>
-          <h5>is work??</h5>
+        <div>
+          <h6>{props.data} the person with this id wants to wager</h6>
+          
 
           
 
-          <div id="incoming-wagers-header" className="MyAccountSubheading">Your Incoming Wagers:</div>
+          
           
         </div>
       )
     } else {
-      navigate('/login')
+    navigate('/login')
     }
 }
 
