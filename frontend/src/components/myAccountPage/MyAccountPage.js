558fe8562a5beae826e7c75b4680a30a5159ea63
Ben <benjdixon96@yahoo.co.uk>
2023-12-14 15:25:08 +0000

    changed incoming wagers to the same format

--- frontend/src/components/myAccountPage/MyAccountPage.js
+++ frontend/src/components/myAccountPage/MyAccountPage.js
@@ -44,9 +44,10 @@ const MyAccountPage = ({ navigate }) => {
           setWagers(data.wagers)
 
           const wagerRequestData = data.wagers.filter(wager => wager.approved === false && wager.peopleInvolved[1] === getSessionUserID(token))
-          setWagerRequests(wagerRequestData)
+          setWagerRequests(wagerRequests)
+          console.log("whyyyyyy")
           
-          setWagerRequests(wagerRequestData)
+         
         
         // console.log(wagers)
         
@@ -76,10 +77,8 @@ const MyAccountPage = ({ navigate }) => {
 
           
           <div id='incoming Wagers' role="incoming wagers">
-          <h4>Incoming Wagers</h4>
-        {wagerRequests.map((wager) => (
-        <IncomingWagers data={wager.peopleInvolved} {...navigate} />
-          ))}
+         
+          <IncomingWagers wagers={wagerRequests}/>
 
           
         <>
--- frontend/src/components/myAccountPage/myAccountPageComponents/IncomingWagers.js
+++ frontend/src/components/myAccountPage/myAccountPageComponents/IncomingWagers.js
@@ -3,20 +3,25 @@ import React, { useEffect, useState } from 'react';
 
 const IncomingWagers = (props, { navigate }) => {
   const [token, setToken] = useState(window.localStorage.getItem("token"));
-  const [data, setData] = useState(props.data)
+  const [wagers, setData] = useState(props.wagers)
+  console.log(props.wagers)
   
 
     if(token) {
       return(
-        <div>
-          <h6>{data} the person with this id wants to wager</h6>
+        <div id="incoming Wagers">
+          
+          
           
 
           
 
           
-          
+          <div id="incoming-wagers-header" className="MyAccountSubheading">Your incoming Wagers:</div>
+        
+          <div>{wagers.map((wager) => (<h6>{wager.peopleInvolved[0]} the person with this Id wants tow wager</h6>))}</div>
         </div>
+      
       )
     } else {
     navigate('/login')
