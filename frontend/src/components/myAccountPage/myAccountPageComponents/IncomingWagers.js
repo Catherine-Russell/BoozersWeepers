a49408f0c559b1886faa5186d68649e60084632d
Ben <benjdixon96@yahoo.co.uk>
2023-12-14 12:56:11 +0000

    added bet data to account Page

--- frontend/src/components/myAccountPage/myAccountPageComponents/IncomingWagers.js
+++ frontend/src/components/myAccountPage/myAccountPageComponents/IncomingWagers.js
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react';
 
-const IncomingWagers = ({ navigate }) => {
+const IncomingWagers = ({ navigate }, props) => {
   const [token, setToken] = useState(window.localStorage.getItem("token"));
+  const [data, setData] = use
 
     if(token) {
       return(
         <div id="incoming-wagers">
+          <h6>{props.id} the person with this id wants to wager</h6>
+          <h5>is work??</h5>
+
+          
 
           <div id="incoming-wagers-header" className="MyAccountSubheading">Your Incoming Wagers:</div>
           
