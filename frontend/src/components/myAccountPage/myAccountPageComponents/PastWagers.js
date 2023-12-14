34976c52ce555246037d6a3ccb5724d78fa0b21e
Catherine Russell <catherinerussell123@hotmail.co.uk>
2023-12-14 14:31:03 +0000

    corrects checkIfOngoing and displays information about ongoing bets in component

--- frontend/src/components/myAccountPage/myAccountPageComponents/PastWagers.js
+++ frontend/src/components/myAccountPage/myAccountPageComponents/PastWagers.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-const HistoricWagers = ({ navigate }) => {
+const HistoricWagers = ({ navigate, pastWagers }) => {
   const [token, setToken] = useState(window.localStorage.getItem("token"));
 
     if(token) {
@@ -8,7 +8,7 @@ const HistoricWagers = ({ navigate }) => {
         <div id="past-wagers">
         
           <div id="past-wagers-header" className="MyAccountSubheading">Your Past Wagers:</div>
-      
+          <div>{pastWagers.map((wager) => (<h6>{wager.description}</h6>))}</div>
         </div>
       )
     } else {
--- frontend/src/components/myAccountPage/myAccountPageComponents/ongoingWagers.js
+++ frontend/src/components/myAccountPage/myAccountPageComponents/ongoingWagers.js
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import "../MyAccountPage.css"
 
-const OngoingWagers = ({ navigate }) => {
+const OngoingWagers = ({ navigate, ongoingWagers }) => {
   const [token, setToken] = useState(window.localStorage.getItem("token"));
 
     if(token) {
       return(
-        <div id="ongoing-wagers">
+        <div id="ongoing-wagers-feed">
           <div id="ongoing-wagers-header" className="MyAccountSubheading">Your Ongoing Wagers:</div>
-        </div>
+          
+            {ongoingWagers.map((wager) => (
+              <div id="ongoing-wager" className='wager'>
+                {wager.peopleInvolved[0]} bet {wager.peopleInvolved[1]} that {wager.description} by {wager.deadline}
+                </div>
+            )
+            )}
+      </div>
       )
     } else {
       navigate('/login')
