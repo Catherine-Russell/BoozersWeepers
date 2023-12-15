59af7d62dd5ae9b625fdfa0926a719cba80db0a1
Catherine Russell <catherinerussell123@hotmail.co.uk>
2023-12-15 16:45:00 +0000

    adds if statements to set up logic for each version of the bet information page

--- frontend/src/components/singlewager/singlewager.js
+++ frontend/src/components/singlewager/singlewager.js
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { navigate, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import getSessionUserID from '../Utility/getSignedInUser_id';
+
 
 const SingleWager = () => {
   const { wagerID } = useParams();
   const [wagerData, setWagerData] = useState(null);
   const [token, setToken] = useState(window.localStorage.getItem('token'));
+  const loggedInUser = getSessionUserID(token)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,15 +29,52 @@ const SingleWager = () => {
       } catch (error) { console.error('Error fetching wager data:', error); }
     };
 
+    const handleAcceptButtonClick = () => {
+      navigate("/myAccount")
+    }
+
     fetchData();
   }, [token, wagerID]);
+
+// New wager request -> if approved == false and who involved[0] != you
+if (wagerData.approved === false && wagerData.peopleInvolved[0] !== loggedInUser) {
 
   return (
     <div>
       <h1>Wager Details</h1>
       {wagerData ? (
-        <div>
+        <div id='wager-request' className='single-wager-info'>
           <p>
+           bets 1 pint that ____ will happen by 12th January <br />Do you accept? 
+            People Involved[0]: {wagerData.wager.peopleInvolved[0].username} <br />
+            People Involved[1]: {wagerData.wager.peopleInvolved[1].username} <br />
+            Quantity: {wagerData.wager.quantity} <br />
+            Approved: {wagerData.wager.approved} <br />
+            {wagerData.wager.winner === null ? (
+              // Code to run when winner is null
+              <>Winner: No winner yet<br /></>
+              ) : (
+                // Code to run when winner is not null
+                <>Winner: {wagerData.wager.winner.username}<br /></>
+                )}
+            Description: {wagerData.wager.description} <br />
+            Deadline: {wagerData.wager.deadline} <br />
+            Date made: {wagerData.wager.datemade} <br />
+          </p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+        )}
+    </div>
+  );
+
+}    // Ongoing wager -> if it has been approved but winner is null
+
+  else if (wagerData.approved === true && wagerData.winner === null) {
+    return (
+      <div id='ongoing-wager-information' className='single-wager-info'>
+        <h1>ongoing wager info</h1>
+        <p>
             Wager ID: {wagerData.wager._id} <br />
             People Involved[0]: {wagerData.wager.peopleInvolved[0].username} <br />
             People Involved[1]: {wagerData.wager.peopleInvolved[1].username} <br />
@@ -43,20 +83,24 @@ const SingleWager = () => {
             {wagerData.wager.winner === null ? (
               // Code to run when winner is null
               <>Winner: No winner yet<br /></>
-            ) : (
-              // Code to run when winner is not null
-              <>Winner: {wagerData.wager.winner.username}<br /></>
-            )}
+              ) : (
+                // Code to run when winner is not null
+                <>Winner: {wagerData.wager.winner.username}<br /></>
+                )}
             Description: {wagerData.wager.description} <br />
             Deadline: {wagerData.wager.deadline} <br />
             Date made: {wagerData.wager.datemade} <br />
           </p>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
-  );
+      </div>
+    )
+  } else if (wagerData.winner !== null) {
+    return (
+      <div id='resolved-wager-information' className='single-wager-info'>
+
+      </div>
+    )
+  }
+
 };
 
 export default SingleWager;
