77eb129cb48fa0fe1cb7bbd1ebcc6fc2d5f80a43
Sam Ford <samueljamesford@googlemail.com>
2023-12-15 12:16:29 +0000

    resolves issues with winner on single wager page

--- frontend/src/components/singlewager/singlewager.js
+++ frontend/src/components/singlewager/singlewager.js
@@ -4,17 +4,20 @@ import { useParams } from 'react-router-dom';
 const SingleWager = () => {
   const { wagerID } = useParams();
   const [wagerData, setWagerData] = useState(null);
-  const [token, setToken] = useState(window.localStorage.getItem("token"));
+  const [token, setToken] = useState(window.localStorage.getItem('token'));
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`/wagers/${wagerID}`, 
-        { headers: { Authorization: `Bearer ${token}` } });
+        const response = await fetch(`/wagers/${wagerID}`, {
+          headers: { Authorization: `Bearer ${token}` }
+        });
 
         console.log('Response status:', response.status);
 
-        if (!response.ok) { throw new Error('Network response was not ok'); }
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
 
         const fetchedData = await response.json();
         console.log('Fetched data:', fetchedData);
@@ -22,7 +25,9 @@ const SingleWager = () => {
         window.localStorage.setItem('token', fetchedData.token);
         setToken(window.localStorage.getItem('token'));
         setWagerData(fetchedData);
-      } catch (error) {console.error('Error fetching wager data:', error);}
+      } catch (error) {
+        console.error('Error fetching wager data:', error);
+      }
     };
 
     fetchData();
@@ -30,22 +35,26 @@ const SingleWager = () => {
 
   return (
     <div>
-      {console.log('Rendering component for wager ID:', wagerID)}
       <h1>Wager Details</h1>
       {wagerData ? (
         <div>
           <p>
-          Wager ID: {wagerData.wager._id} <br/>
-          People Involved[0]: {wagerData.wager.peopleInvolved[0].username} <br/>
-          People Involved[1]: {wagerData.wager.peopleInvolved[1].username} <br/>
-          Quantity: {wagerData.wager.quantity} <br/>
-          Approved: {wagerData.wager.approved} <br/>
-          Winner: {wagerData.wager.winner.username} <br/>
-          Description: {wagerData.wager.description} <br/>
-          Deadline: {wagerData.wager.deadline} <br/>
-          Date made: {wagerData.wager.datemade} <br/>
+            Wager ID: {wagerData.wager._id} <br />
+            People Involved[0]: {wagerData.wager.peopleInvolved[0].username} <br />
+            People Involved[1]: {wagerData.wager.peopleInvolved[1].username} <br />
+            Quantity: {wagerData.wager.quantity} <br />
+            Approved: {wagerData.wager.approved} <br />
+            {wagerData.wager.winner === null ? (
+              // Code to run when winner is null
+              <>Winner: No winner yet<br /></>
+            ) : (
+              // Code to run when winner is not null
+              <>Winner: {wagerData.wager.winner.username}<br /></>
+            )}
+            Description: {wagerData.wager.description} <br />
+            Deadline: {wagerData.wager.deadline} <br />
+            Date made: {wagerData.wager.datemade} <br />
           </p>
-
         </div>
       ) : (
         <p>Loading...</p>
