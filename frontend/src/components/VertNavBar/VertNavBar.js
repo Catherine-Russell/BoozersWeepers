42ed50c92a3bda18a38dd40e2d0e66b8d101a61f
Sam Ford <samueljamesford@googlemail.com>
2023-12-18 12:18:14 +0000

    Adds ReactICONS to vert navbar

--- frontend/src/components/VertNavBar/VertNavBar.js
+++ frontend/src/components/VertNavBar/VertNavBar.js
@@ -1,32 +1,24 @@
 import React from 'react';
+import {useNavigate} from "react-router-dom";
 import { IoHome,IoPint } from 'react-icons/io5';
+import { BiExpandHorizontal } from "react-icons/bi";
 import '/Users/samuelford/Projects/TeamTavern/frontend/src/components/VertNavBar/VertNavBar.css'
+
 
 const VertNavbar = ({ expanded, toggleExpand }) => {
   return (
     <nav className={`VertNavbar ${expanded ? 'expanded' : ''}`}>
-      <div className="top-section">
-        <div className="VertNavbar-logo" onClick={toggleExpand}>
-          <span>Logo</span>
-        </div>
         <div className="VertNavbar-links">
           <ul>
-            <li>
-              <a href='/'><span>Home</span>
-              <IoHome className="react-icon" size={30} /></a>
-            </li>
-            <li>
-              <span>New Bet</span>
-              <IoPint className="react-icon" size={30} />
-            </li>
-            <li>
-              <span>Option 3</span>
-              <IoPint className="react-icon" size={30} />
-            </li>
+          <li onClick={toggleExpand}><BiExpandHorizontal className="react-icon" size={30} /></li>
+          <li className="spacer"></li> {/* Empty list item acting as a spacer */}
+          <li><a href='/'><span>Home</span><IoHome className="react-icon" size={30} /></a></li>
+          <li><span>New Bet</span><IoPint className="react-icon" size={30} onClick={useNavigate('/signup')}/></li>
+          <li><span>Option 3</span><IoPint className="react-icon" size={30} /></li>
             {/* Add more options as needed */}
           </ul>
         </div>
-      </div>
+
       <div className="bottom-section">
         <div>Item at the bottom</div>
       </div>
