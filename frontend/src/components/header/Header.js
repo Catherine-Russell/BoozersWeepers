658ee8aea169de7db1abd23e72d3ad191ef32501
Sam Ford <samueljamesford@googlemail.com>
2023-12-18 22:42:10 +0000

    Adds Header Logo and Favicon

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import '../header/header.css';
+import LogoGraphic from '../../Assets/BoozersWeepersLogo_trans.png'
 
 const Header = () => {
     return (
         <div className='HeaderContainer'> 
-          <div className="Spacer" style={{ height: '20px' }}></div>
-          <div className='HeaderTopBar'></div>
-          <div className="Spacer" style={{ height: '5px' }}></div>
+          
           <div className="HeaderMainBar"><a href='/'>
-        <img src='https://www.movieboozer.com/wp-content/uploads/2020/05/Movieboozer-logo-1200x630-1.png' alt='BoozersWeepers Logo'/></a></div>
+        <img src={LogoGraphic} alt='BoozersWeepers Logo'/></a></div>
         </div>
       );
     };
