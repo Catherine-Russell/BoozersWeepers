8bdc9c54f78b5eacbff8394592e31df6fba8d2e3
Catherine Russell <catherinerussell123@hotmail.co.uk>
2023-12-20 09:00:41 +0000

    updates single pending wager component with information and button to cancel the request (needs backend cancel function)

--- frontend/src/components/singleWagerPage/childComponents/SinglePendingWager.js
+++ frontend/src/components/singleWagerPage/childComponents/SinglePendingWager.js
@@ -1,7 +1,15 @@
 const SinglePendingWager = (wagerData) => {
+    const wager = wagerData.wagerData
+    const dateParts = wager.deadline.slice(0, 10).split("-");
+    const deadlineDate = `${dateParts[2]}/${dateParts[1]}/${dateParts[0]}`
+
+    const handleCancelButtonClick = () => {
+        console.log("cancel button clicked")
+    }
     return (
         <div id='single-pending-wager' className="single-wager-info">
-            This is a pending wager
+            Waiting for {wager.peopleInvolved[1].username} to respond to your wager that {wager.description} by {deadlineDate} <br />
+            <button id='cancel-request-button' onClick={ handleCancelButtonClick }>Cancel Request</button>
         </div>
     )
 }
