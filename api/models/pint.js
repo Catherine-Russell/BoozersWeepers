092239157390c3f639311c6e15659bc387a43107
Sam Ford <samueljamesford@googlemail.com>
2024-01-07 11:39:49 +0000

    Adds boolean claimed to pint schema

--- api/models/pint.js
+++ api/models/pint.js
@@ -4,7 +4,8 @@ const PintSchema = new mongoose.Schema({
   owner:{type: mongoose.Schema.Types.ObjectId,ref: 'User',default: null},
   datemade: {type: Date, default: new Date()},
   owed_by:{type: mongoose.Schema.Types.ObjectId,ref: 'User',default: null},
-  bet:{type: mongoose.Schema.Types.ObjectId,ref: 'Wager',default: null}
+  bet:{type: mongoose.Schema.Types.ObjectId,ref: 'Wager',default: null},
+  claimed:{type: Boolean, default: false},
 });
 
 const Pint = mongoose.model("Pint", PintSchema);
