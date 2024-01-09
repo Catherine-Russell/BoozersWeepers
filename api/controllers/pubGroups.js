72ea6d394cdf6a5432f903285cff5538e02d6b36
Catherine Russell <catherinerussell123@hotmail.co.uk>
2024-01-09 11:13:55 +0000

    adds UpdateAddNewMember function to pubGroupController

--- api/controllers/pubGroups.js
+++ api/controllers/pubGroups.js
@@ -18,31 +18,53 @@ const PubGroupsController = {
 		})
 	},
 
-		Index: (req, res) => {
-			PubGroup.find((err, pubGroups) => {
-				if (err) {
-					return res.status(500).json({ error: 'Internal Server Error' });
+	UpdateAddMember: async (req, res) => {
+			// Finds the pubGroup to update
+			try{
+				const pubGroupId = req.params;
+				const userId = req.user_id;
+				if (!pubGroupId || !userId) {
+					return res.status(400).json({ error: 'Both Pub Group ID and user ID are required.' });
 				}
-				const token = TokenGenerator.jsonwebtoken(req.user_id);
-				return res.status(200).json({ pubGroups: pubGroups, token: token });
-			});
-		},
+				const existingPubGroup = await PubGroup.findById(pubGroupId);
+				if (!existingPubGroup) {
+					return res.status(404).json({ error: 'Group not found.' });
+				}
+	
+				// Add member
+				await PubGroup.updateOne({ _id: pubGroupId }, { $push: { member: userId } });
+				res.status(200).json({ message: 'Member added successfully.' });
+			} catch (error) {
+				console.error('Error adding new member:', error);
+				res.status(500).json({ error: 'Internal Server Error.' });
+			}
+	}
 
-		FindInfoByGroupID: (req, res) => {
-			const pubGroupID = req.params.id;
-			PubGroup.findById(pubGroupID)
-			// .populate('peopleInvolved winner')
-			.exec((err, pubGroup) => {
-					if (err) {
-						return res.status(500).json({ error: 'Internal Server Error' });
-					}
-					if (!pubGroup) {
-						return res.status(404).json({ error: 'Pub Group not found' });
-					}
-					const token = TokenGenerator.jsonwebtoken(req.user_id);
-					return res.status(200).json({ pubGroup: pubGroup, token: token });
-				});
-		},
+		// Index: (req, res) => {
+		// 	PubGroup.find((err, pubGroups) => {
+		// 		if (err) {
+		// 			return res.status(500).json({ error: 'Internal Server Error' });
+		// 		}
+		// 		const token = TokenGenerator.jsonwebtoken(req.user_id);
+		// 		return res.status(200).json({ pubGroups: pubGroups, token: token });
+		// 	});
+		// },
+
+		// FindInfoByGroupID: (req, res) => {
+		// 	const pubGroupID = req.params.id;
+		// 	PubGroup.findById(pubGroupID)
+		// 	// .populate('peopleInvolved winner')
+		// 	.exec((err, pubGroup) => {
+		// 			if (err) {
+		// 				return res.status(500).json({ error: 'Internal Server Error' });
+		// 			}
+		// 			if (!pubGroup) {
+		// 				return res.status(404).json({ error: 'Pub Group not found' });
+		// 			}
+		// 			const token = TokenGenerator.jsonwebtoken(req.user_id);
+		// 			return res.status(200).json({ pubGroup: pubGroup, token: token });
+		// 		});
+		// },
 
 };
 
