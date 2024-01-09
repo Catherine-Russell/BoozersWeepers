60528df8ebf5e20f2b32dc1e7a6dda0cc810a068
Catherine Russell <catherinerussell123@hotmail.co.uk>
2024-01-09 15:43:50 +0000

    pubGroupsPage gets list of pubGroups from the database and displays them on page

--- frontend/src/components/pubGroupsPage/PubGroupsPage.js
+++ frontend/src/components/pubGroupsPage/PubGroupsPage.js
@@ -1,27 +1,60 @@
 import VertNavbar from "../VertNavBar/VertNavBar";
+import isTokenValid from '../Utility/isTokenValid';
 import React, { useEffect, useState } from 'react';
-
-const PubGroupsPage = () => {
-    const [expanded, setExpanded] = useState(true);
-
-    const toggleExpand = () => {setExpanded(!expanded);};
+import '../../Pages/style.css'
 
 
-    return(
-        <div className="pub-groups-page-container">
-        {/* <VertNavbar expanded={expanded} toggleExpand={toggleExpand} /> */}
-				
-				<div className="my-groups">
-					<h1>My groups:</h1>
+const PubGroupsPage = ({ navigate }) => {
+	const [token, setToken] = useState(window.localStorage.getItem("token"));
+  const [pubGroups, setPubGroups] = useState([])
+  const [isLoggedIn, setIsLoggedIn] = useState(isTokenValid(token));
+  const [expanded, setExpanded] = useState(true);
+
+  const toggleExpand = () => {setExpanded(!expanded);};
+
+
+	useEffect((event) => {
+    
+    // Gets pub groups data from backend
+    if(token) {
+      fetch("/pubGroups", {
+        method: 'get',
+        headers: {'Authorization': `Bearer ${token}`}
+      })
+        .then(response => response.json())
+        .then(async data => {
+          window.localStorage.setItem("token", data.token)
+          setToken(window.localStorage.getItem("token"))
+          setPubGroups(data.pubGroups)
+        })
+      }
+			if (!isLoggedIn) {navigate('/');}
+    }, [navigate, isLoggedIn, token]);
+
+  return(
+    <div className="pub-groups-page-container">
+    {/* <VertNavbar expanded={expanded} toggleExpand={toggleExpand} /> */}
+		
+			<div className="my-groups">
+				<h1>My groups:</h1>
 					list of groups
-				</div>
-				<br></br>
-				<div className="new-groups">
-					put search bar here and button to go to that individual page
-				</div>
+
+					{pubGroups.map((pubGroup) => (
+					<div key={pubGroup.id}>
+						
+					<a href={`/pubGroup/${pubGroup._id}`} >
+						these are the pubgroups: {pubGroup.name}?
+					</a>
+					</div>))}
+
+			</div>
+		<br></br>
+			<div className="new-groups">
+			put search bar here and button to go to that individual page
+			</div>
             
-        </div>
-    )
+    </div>
+	)
 }
 
 export default PubGroupsPage;
