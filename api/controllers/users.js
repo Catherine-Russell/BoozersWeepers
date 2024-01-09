66c4d7a41c2dde1fb270b50c7a0c435135110475
Rachel Roberts <rachelroberts_sw@yahoo.co.uk>
2024-01-09 16:07:36 +0000

    backend user controller updated to add password validation (min 8 chars and min 1 special character)

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -3,16 +3,29 @@ const User = require("../models/user");
 const UsersController = {
   Create: (req, res) => {
     const user = new User(req.body);
+    let passwordErrorMsgs = "";
+
+    if(user.password.length < 8) {
+      passwordErrorMsgs += "Password must be at least 8 characters in length. \n"
+    } 
+    if(!/[!@£#$%^&*()"'-_+={}\[\]:;<>,.?~\\/|]/.test(user.password)) {
+      passwordErrorMsgs += "Password must contain at least one special character. \n";
+    } 
+    if (passwordErrorMsgs !== "") {
+      return res.status(400).json({message: passwordErrorMsgs})
+    } 
+    else {
+
     user.save((err) => {
       if (err) {
         
         if(err.code === 11000){
           if(err.keyPattern.email){
-            return res.status(400).json({message: 'email is already in use'})
+            return res.status(400).json({message: 'Email is already in use'})
 
           }
           else if(err.keyPattern.username)
-          return res.status(400).json({ message: 'username is already in use' });
+          return res.status(400).json({ message: 'Username is already in use' });
 
         }
         else{
@@ -25,7 +38,7 @@ const UsersController = {
         res.status(201).json({ message: 'OK' });
       }
     });
-  }
+  }}
 };
 
 module.exports = UsersController;
