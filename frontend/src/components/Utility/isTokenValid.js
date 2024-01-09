db666de01991f68f73611891ee2103361133a3e2
Sam Ford <samueljamesford@googlemail.com>
2024-01-09 16:00:37 +0000

    Resolves DOM Error String

--- frontend/src/components/Utility/isTokenValid.js
+++ frontend/src/components/Utility/isTokenValid.js
@@ -1,12 +1,25 @@
 function isTokenValid(token) {
-    if (!token) {return false;}
-
-    const tokenPayload = JSON.parse(atob(token.split('.')[1]));
-    if (!tokenPayload.exp) {return false;}
-
-    const expirationTime = tokenPayload.exp * 1000; 
-    const currentTime = new Date().getTime(); 
-    return currentTime < expirationTime;
+  if (!token) {
+      return false;
   }
 
-  export default isTokenValid;
+  const tokenPayload = token.split('.')[1];
+  try {
+      const decodedToken = decodeURIComponent(atob(tokenPayload).split('').map(c => {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      }).join(''));
+
+      const parsedPayload = JSON.parse(decodedToken);
+
+      if (!parsedPayload.exp) {return false;}
+
+      const expirationTime = parsedPayload.exp * 1000;
+      const currentTime = new Date().getTime();
+      return currentTime < expirationTime;
+  } catch (error) {
+      console.error('Error decoding token:', error);
+      return false;
+  }
+}
+
+export default isTokenValid;
