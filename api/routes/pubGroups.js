69f7ea0cb819388d0cca6d193e23217de13111bf
Catherine Russell <catherinerussell123@hotmail.co.uk>
2024-01-10 16:10:26 +0000

    adds new removeMember route

--- api/routes/pubGroups.js
+++ api/routes/pubGroups.js
@@ -6,6 +6,7 @@ const PubGroupsController = require("../controllers/pubGroups");
 router.get("/:pubGroupId", PubGroupsController.FindMemberInfoByPubGroupID);
 router.get("/", PubGroupsController.Index);
 router.post("/:pubGroupId/addMember", PubGroupsController.UpdateAddMember)
+router.post("/:pubGroupId/removeMember", PubGroupsController.UpdateRemoveMember)
 router.post("/", PubGroupsController.Create);
 
 module.exports = router;
